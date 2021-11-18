c42199b09efac12fa2a4f292155b61471dc3d63e
Bunty Chakraborty <buntychakraborty@outlook.com>
2021-11-18 10:20:46 +0530

    code refactoring

--- src/main/java/Employee/management/EmployeeManagementSystem/controller/EmployeeController.java
+++ src/main/java/Employee/management/EmployeeManagementSystem/controller/EmployeeController.java
@@ -6,7 +6,8 @@ import Employee.management.EmployeeManagementSystem.repository.EmployeeRepositor
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,54 +15,54 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	// Get all Employees
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees() {
+		logger.info("Employees Api is accessed");
+		return employeeRepository.findAll();
+	}
 
-    //Get all Employees
-    @GetMapping("/employees")
-    public List<Employee> getAllEmployees() {
-        System.out.println("Employees Api is accessed");
-        return employeeRepository.findAll();
-    }
+	// Create Employee Rest API
+	@PostMapping("/create")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
 
-    //Create Employee Rest API
-    @PostMapping("/create")
-    public Employee createEmployee(@RequestBody Employee employee) {
-        return employeeRepository.save(employee);
-    }
+	// Get Employees By Id
+	@GetMapping("/employee/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee Doesn't Exist"));
+		return ResponseEntity.ok(employee);
 
-    //Get Employees By Id
-    @GetMapping("/employee/{id}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
-        Employee employee = employeeRepository.findById(id)
-                .orElseThrow( () -> new ResourceNotFoundException("Employee Doesn't Exist"));
-        return ResponseEntity.ok(employee);
+	}
 
-    }
+	// Updating the Employee
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee Not Found"));
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setPhoneNumber(employeeDetails.getPhoneNumber());
+		employee.setEmailId(employeeDetails.getEmailId());
+		Employee updatedEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok(updatedEmployee);
+	}
 
-    //Updating the Employee
-    @PutMapping("/update/{id}")
-    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
-        Employee employee = employeeRepository.findById(id)
-                .orElseThrow( () -> new ResourceNotFoundException("Employee Not Found"));
-        employee.setFirst_name(employeeDetails.getFirst_name());
-        employee.setLast_name(employeeDetails.getLast_name());
-        employee.setPhone_number(employeeDetails.getPhone_number());
-        employee.setEmail_id(employeeDetails.getEmail_id());
-        Employee updatedEmployee = employeeRepository.save(employee);
-
-        return ResponseEntity.ok(updatedEmployee);
-    }
-
-    //Deleting the Employee
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
-        Employee employee = employeeRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Employee Not Found"));
-        employeeRepository.delete(employee);
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("Deleted", Boolean.TRUE);
-        return ResponseEntity.ok(response);
-    }
- }
+	// Deleting the Employee
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee Not Found"));
+		employeeRepository.delete(employee);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("Deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
+}
--- src/main/java/Employee/management/EmployeeManagementSystem/model/Employee.java
+++ src/main/java/Employee/management/EmployeeManagementSystem/model/Employee.java
@@ -3,73 +3,52 @@ package Employee.management.EmployeeManagementSystem.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "Employees")
+@Table(name = "employee_tbl")
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
-    @Column(name = "First_Name")
-    private String first_name;
-
-    @Column(name = "LastName")
-    private String last_name;
-
-    @Column(name = "email_id")
-    private String email_id;
-
-    @Column(name = "phone_number")
-    private String phone_number;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String phoneNumber;
 
     public long getId() {
         return id;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public String getFirst_name() {
-        return first_name;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public String getLast_name() {
-        return last_name;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
+	public String getEmailId() {
+		return emailId;
+	}
 
-    public String getEmail_id() {
-        return email_id;
-    }
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
 
-    public void setEmail_id(String email_id) {
-        this.email_id = email_id;
-    }
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
 
-    public String getPhone_number() {
-        return phone_number;
-    }
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
-    public void setPhone_number(String phone_number) {
-        this.phone_number = phone_number;
-    }
-
-    public Employee() {
-    }
-
-    public Employee(String first_name, String last_name, String email_id, String phone_number) {
-        super();
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.email_id = email_id;
-        this.phone_number = phone_number;
-    }
+    
 }
